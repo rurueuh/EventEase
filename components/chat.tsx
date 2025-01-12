2a21fcff36363f41f5de0e2e34d77e06c96a5ec8
Ruru <maximilien.vanstaevel@epitech.eu>
2025-01-13 01:07:30 +0900

    refactor chat component to enhance UI with card layout and improved message display

--- components/chat.tsx
+++ components/chat.tsx
@@ -2,7 +2,10 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { collection, query, onSnapshot, orderBy, getDocs, doc, where, Timestamp, updateDoc, getDoc, Query, DocumentData } from "firebase/firestore";
 import { db } from "@/firebase";
-import { randomBytes} from "crypto";
+import { randomBytes } from "crypto";
+import { Textarea } from "@nextui-org/input";
+import { Card, CardBody, Chip, Spacer } from "@nextui-org/react";
+import Image from "next/image";
 
 type Props = {
     eventID: string;
@@ -10,8 +13,8 @@ type Props = {
 };
 
 interface Message {
-    uid: string,
     useruid: string,
+    username: string,
     message: string,
     timestamps: Timestamp
 }
@@ -31,7 +34,7 @@ export const Chat: React.FC<Props> = ({ eventID, userId }: Props) => {
         const unsubscribe = subscribeToChat(q, setMessages)
 
         const q2 = query(collection(db, "chats"), where("eventID", "==", eventID));
-    
+
         fetchAndSetChatMessages(q2, setMessages);
         return () => unsubscribe();
     }, [messages, setMessages])
@@ -41,39 +44,53 @@ export const Chat: React.FC<Props> = ({ eventID, userId }: Props) => {
             return;
         if (myMessage.trim() === "") return;
 
-    
+
         getDoc(doc(db, "chats", eventID)).then((snap) => {
             let chat = snap.data() as ChatInterface;
             let newMessage = {} as Message;
             newMessage.message = myMessage;
             newMessage.useruid = userId;
             newMessage.timestamps = Timestamp.now()
-            newMessage.useruid = "magic useless"
             chat.messages.push(newMessage)
             updateDoc(doc(db, "chats", eventID), { ...chat })
             setMyMessage("");
         })
-      };
+    };
 
     if (!messages) {
         return (<></>)
     }
 
     return (
-        <div>
-            <div>
-                {messages.map((m) => (
-                    <p key={m.uid}>{m.timestamps.toDate().toDateString()}:{m.message}</p>
-                ))}
-            </div>
-            <input
-                type="text"
-                value={myMessage}
-                onChange={(e) => setMyMessage(e.target.value)}
-                placeholder="Écrire un message..."
-            />
-            <button onClick={sendMessage}>Envoyer</button>
-        </div>
+        <Card>
+            <CardBody>
+                <div className="flex flex-col max-h-[500px] overflow-y-scroll  will-change-scroll">
+                    {messages.map((m) => (
+                        <Chip radius="lg" className="h-max p-2 m-2" color={m.useruid === userId ? "primary" : "default"} key={randomBytes(1024) as unknown as string}>
+                            <div className="flex">
+                                <div className=" min-w-[36px] mr-3 mt-0.5">
+                                    <Image height={36} width={36} alt={"profile picture of".concat("e")} src="/profile.webp" />
+                                </div>
+                                <div className="max-w-full">
+                                    <p className=" text-wrap">{m.message}</p>
+                                    <p className="text-[0.5rem] text-slate-300">de {m.username ?? "inconnu"}, le {m.timestamps.toDate().toUTCString()}</p>
+                                </div>
+                            </div>
+                        </Chip>
+                    ))}
+                </div>
+                <Spacer y={3} />
+                <Textarea
+                    isClearable
+                    placeholder="Écrire un message..."
+                    variant="bordered"
+                    value={myMessage}
+                    onChange={(e) => setMyMessage(e.target.value)}
+                    onClear={() => setMyMessage("")}
+                />
+                <button onClick={sendMessage}>Envoyer</button>
+            </CardBody>
+        </Card>
     );
 }
 
@@ -91,15 +108,13 @@ function subscribeToChat(q: Query<DocumentData, DocumentData>, setMessages: Disp
 }
 
 function fetchAndSetChatMessages(
-    q2 : Query<DocumentData, DocumentData>,
+    q2: Query<DocumentData, DocumentData>,
     setMessages: Dispatch<SetStateAction<Message[]>>
 ) {
     getDocs(q2).then((snap) => {
         snap.forEach((doc) => {
             let chat = doc.data() as ChatInterface;
-            chat.messages.forEach((m) => {
-                m.uid = randomBytes(1024) as unknown as string;
-            });
+
             setMessages(chat.messages);
         });
     });
