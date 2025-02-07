347c9459d562e65107cb645a7f8260b13e89ad85
Ruru <maximilien.vanstaevel@epitech.eu>
2025-02-08 02:33:38 +0900

    fix: update navbar branding and correct site links

--- config/site.ts
+++ config/site.ts
@@ -56,10 +56,10 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
+    github: "https://github.com/rurueuh/EventEase",
     twitter: "https://twitter.com/getnextui",
     docs: "https://nextui.org",
     discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    sponsor: "https://github.com/rurueuh/EventEase",
   },
 };
